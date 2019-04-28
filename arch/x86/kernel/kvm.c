8f61a8f0a51c4d194c6835ed182d8ca4140c7e34
jxwufan <jxwufan@node.jxwufan-qv51217.kvmarm-pg0.wisc.cloudlab.us>
2019-04-28 14:35:59 -0500

    disable paravirtualization

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -587,10 +587,9 @@ static noinline uint32_t __kvm_cpuid_base(void)
 	if (boot_cpu_data.cpuid_level < 0)
 		return 0;	/* So we don't blow up on old processors */
 
+	return 0;
 	if (boot_cpu_has(X86_FEATURE_HYPERVISOR))
 		return hypervisor_cpuid_base("KVMKVMKVM\0\0\0", 0);
-
-	return 0;
 }
 
 static inline uint32_t kvm_cpuid_base(void)
